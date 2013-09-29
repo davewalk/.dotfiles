6970842c4440d41f3fc5a489745e447514203d42
Dave Walk <daviddwalk@gmail.com>
2013-09-28 23:02:56 -0400

    Sublime updates

---
+++ Sublime Text 2/Packages/WakaTime/WakaTime.py
@@ -0,0 +1,198 @@
+""" ==========================================================
+File:        WakaTime.py
+Description: Automatic time tracking for Sublime Text 2 and 3.
+Maintainer:  WakaTi.me <support@wakatime.com>
+Website:     https://www.wakati.me/
+==========================================================="""
+
+__version__ = '1.3.7'
+
+import sublime
+import sublime_plugin
+
+import glob
+import os
+import platform
+import sys
+import time
+import threading
+import uuid
+from os.path import expanduser, dirname, realpath, isfile, join, exists
+
+
+# globals
+ACTION_FREQUENCY = 5
+ST_VERSION = int(sublime.version())
+PLUGIN_DIR = dirname(realpath(__file__))
+API_CLIENT = '%s/packages/wakatime/wakatime-cli.py' % PLUGIN_DIR
+SETTINGS_FILE = 'WakaTime.sublime-settings'
+SETTINGS = {}
+LAST_ACTION = 0
+LAST_FILE = None
+HAS_SSL = False
+LOCK = threading.RLock()
+
+# check if we have SSL support
+try:
+    import ssl
+    HAS_SSL = True
+
+    # import wakatime package
+    sys.path.insert(0, join(PLUGIN_DIR, 'packages', 'wakatime'))
+    import wakatime
+
+except ImportError:
+    from subprocess import Popen
+
+
+def setup_settings_file():
+    """ Convert ~/.wakatime.conf to WakaTime.sublime-settings
+    """
+    global SETTINGS
+    # To be backwards compatible, rename config file
+    SETTINGS = sublime.load_settings(SETTINGS_FILE)
+    api_key = SETTINGS.get('api_key', '')
+    if not api_key:
+        api_key = ''
+        try:
+            with open(join(expanduser('~'), '.wakatime.conf')) as old_file:
+                for line in old_file:
+                    line = line.split('=', 1)
+                    if line[0] == 'api_key':
+                        api_key = str(line[1].strip())
+            try:
+                os.remove(join(expanduser('~'), '.wakatime.conf'))
+            except:
+                pass
+        except IOError:
+            pass
+    SETTINGS.set('api_key', api_key)
+    sublime.save_settings(SETTINGS_FILE)
+
+
+def prompt_api_key():
+    global SETTINGS
+    if not SETTINGS.get('api_key'):
+        def got_key(text):
+            if text:
+                SETTINGS.set('api_key', str(text))
+                sublime.save_settings(SETTINGS_FILE)
+        window = sublime.active_window()
+        if window:
+            window.show_input_panel('Enter your WakaTime api key:', '', got_key, None, None)
+            return True
+        else:
+            print('Error: Could not prompt for api key because no window found.')
+    return False
+
+
+def python_binary():
+    python = 'python'
+    if platform.system() == 'Windows':
+        python = 'pythonw'
+        try:
+            Popen([python, '--version'])
+        except:
+            for path in glob.iglob('/python*'):
+                if exists(realpath(join(path, 'pythonw.exe'))):
+                    python = realpath(join(path, 'pythonw'))
+                    break
+    return python
+
+
+def enough_time_passed(now):
+    if now - LAST_ACTION > ACTION_FREQUENCY * 60:
+        return True
+    return False
+
+
+def handle_write_action(view):
+    global LOCK, LAST_FILE, LAST_ACTION
+    with LOCK:
+        targetFile = view.file_name()
+        thread = SendActionThread(targetFile, isWrite=True)
+        thread.start()
+        LAST_FILE = targetFile
+        LAST_ACTION = time.time()
+
+
+def handle_normal_action(view):
+    global LOCK, LAST_FILE, LAST_ACTION
+    with LOCK:
+        targetFile = view.file_name()
+        thread = SendActionThread(targetFile)
+        thread.start()
+        LAST_FILE = targetFile
+        LAST_ACTION = time.time()
+
+
+class SendActionThread(threading.Thread):
+
+    def __init__(self, targetFile, isWrite=False, force=False):
+        threading.Thread.__init__(self)
+        self.targetFile = targetFile
+        self.isWrite = isWrite
+        self.force = force
+        self.debug = SETTINGS.get('debug')
+        self.api_key = SETTINGS.get('api_key', '')
+        self.last_file = LAST_FILE
+
+    def run(self):
+        if self.targetFile:
+            self.timestamp = time.time()
+            if self.force or self.isWrite or self.targetFile != self.last_file or enough_time_passed(self.timestamp):
+                self.send()
+
+    def send(self):
+        if not self.api_key:
+            print('missing api key')
+            return
+        ua = 'sublime/%d sublime-wakatime/%s' % (ST_VERSION, __version__)
+        cmd = [
+            API_CLIENT,
+            '--file', self.targetFile,
+            '--time', str('%f' % self.timestamp),
+            '--plugin', ua,
+            '--key', str(bytes.decode(self.api_key.encode('utf8'))),
+        ]
+        if self.isWrite:
+            cmd.append('--write')
+        if self.debug:
+            cmd.append('--verbose')
+            print(cmd)
+        if HAS_SSL:
+            wakatime.main(cmd)
+        else:
+            cmd.insert(0, python_binary())
+            if platform.system() == 'Windows':
+                Popen(cmd, shell=False)
+            else:
+                with open(join(expanduser('~'), '.wakatime.log'), 'a') as stderr:
+                    Popen(cmd, stderr=stderr)
+
+
+def plugin_loaded():
+    setup_settings_file()
+    after_loaded()
+
+
+def after_loaded():
+    if not prompt_api_key():
+        sublime.set_timeout(after_loaded, 500)
+
+
+# need to call plugin_loaded because only ST3 will auto-call it
+if ST_VERSION < 3000:
+    plugin_loaded()
+
+
+class WakatimeListener(sublime_plugin.EventListener):
+
+    def on_post_save(self, view):
+        handle_write_action(view)
+
+    def on_activated(self, view):
+        handle_normal_action(view)
+
+    def on_modified(self, view):
+        handle_normal_action(view)
---
+++ Sublime Text 2/Packages/WakaTime/packages/wakatime/wakatime/projects/git.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+    wakatime.projects.git
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Information about the git project for a given file.
+
+    :copyright: (c) 2013 Alan Hamlett.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import logging
+import os
+from subprocess import Popen, PIPE
+
+from .base import BaseProject
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ..packages.ordereddict import OrderedDict
+
+
+log = logging.getLogger(__name__)
+
+
+class Git(BaseProject):
+
+    def process(self):
+        self.config = self._find_config(self.path)
+        if self.config:
+            return True
+        return False
+
+    def name(self):
+        base = self._project_base()
+        if base:
+            return os.path.basename(base)
+        return None
+
+    def branch(self):
+        stdout = None
+        try:
+            stdout, stderr = Popen([
+                    'git', 'branch', '--no-color'
+                ], stdout=PIPE, stderr=PIPE, cwd=self._project_base()
+            ).communicate()
+        except OSError:
+            pass
+        if stdout:
+            for line in stdout.splitlines():
+                if isinstance(line, bytes):
+                    line = bytes.decode(line)
+                line = line.split(' ', 1)
+                if line[0] == '*':
+                    return line[1]
+        return None
+
+
+    def _project_base(self):
+        if self.config:
+            return os.path.dirname(os.path.dirname(self.config))
+        return None
+
+    def _find_config(self, path):
+        path = os.path.realpath(path)
+        if os.path.isfile(path):
+            path = os.path.split(path)[0]
+        if os.path.isfile(os.path.join(path, '.git', 'config')):
+            return os.path.join(path, '.git', 'config')
+        split_path = os.path.split(path)
+        if split_path[1] == '':
+            return None
+        return self._find_config(split_path[0])
+
+    def _parse_config(self):
+        sections = {}
+        try:
+            f = open(self.config, 'r')
+        except IOError as e:
+            log.exception("Exception:")
+        else:
+            with f:
+                section = None
+                for line in f.readlines():
+                    line = line.lstrip()
+                    if len(line) > 0 and line[0] == '[':
+                        section = line[1:].split(']', 1)[0]
+                        temp = section.split(' ', 1)
+                        section = temp[0].lower()
+                        if len(temp) > 1:
+                            section = ' '.join([section, temp[1]])
+                        sections[section] = {}
+                    else:
+                        try:
+                            (setting, value) = line.split('=', 1)
+                        except ValueError:
+                            setting = line.split('#', 1)[0].split(';', 1)[0]
+                            value = 'true'
+                        setting = setting.strip().lower()
+                        value = value.split('#', 1)[0].split(';', 1)[0].strip()
+                        sections[section][setting] = value
+                f.close()
+        return sections
